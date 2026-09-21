45504d2e7b9edfa9f265f87a0f11281244f82f9d
opueyo <opueyo@bgeo.es>
2026-09-21 14:58:44 +0200

    chore: update version

--- src/hydraulic_engine/__init__.py
+++ src/hydraulic_engine/__init__.py
@@ -10,7 +10,7 @@ from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("hydraulic-engine")
 except PackageNotFoundError:
-    __version__ = "0.10.0"
+    __version__ = "0.10.1"
 
 __author__ = "BGEO"
 __email__ = "info@bgeo.es"
